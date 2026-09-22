8363470090f27cd8421e29ac9d8be6302fb719e6
Vinay Dasari <dasariv211@gmail.com>
2026-09-22 21:18:28 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -77,3 +77,42 @@ resource "terraform_data" "redis" {
     ]
   }
 }
+resource "aws_instance" "rabbitmq" {
+  ami           = data.aws_ami.joindevops.id
+  instance_type = "t3.micro"
+  # Pass the security group ID as a list
+  vpc_security_group_ids = [local.rabbitmq_sg_id]
+  subnet_id              = local.databse_subnet_id
+  #   key_name               = "daws-key"
+  #   user_data = templatefile("${path.module}/bastion.sh.tftpl", {
+  #     partition_number = 4
+  #     extend_size      = 30
+  #   })
+  tags = merge(
+    {
+      Name = "${local.common_name}-rabbitmq"
+    },
+    local.common_tags
+  )
+}
+resource "terraform_data" "rabbitmq" {
+  triggers_replace = [
+    aws_instance.rabbitmq.id
+  ]
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.rabbitmq.private_ip
+  }
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+  provisioner "remote-exec" {
+   inline = [ 
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh rabbitmq ${var.environment}"
+    ]
+  }
+}
