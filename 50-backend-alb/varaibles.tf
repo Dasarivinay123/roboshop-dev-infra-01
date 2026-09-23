66676a8d114a6abfddfb8da8b8a4ba8d03aafdf2
Vinay Dasari <dasariv211@gmail.com>
2026-09-23 13:50:42 +0530

    terraform

---
+++ 50-backend-alb/varaibles.tf
@@ -0,0 +1,18 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_id" {
+  default = "Z0259442CKV1OWK97ZR9"
+}
+
+variable "domain_name" {
+  default = "vinaydevops.online"
+}
+variable "mysql_root_password" {
+  type = string
+}
