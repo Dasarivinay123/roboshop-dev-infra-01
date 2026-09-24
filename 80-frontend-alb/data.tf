ce6a0833a665a47592ec6083872c8302cadaa1fe
Vinay Dasari <dasariv211@gmail.com>
2026-09-24 16:39:40 +0530

    terraform

---
+++ 80-frontend-alb/data.tf
@@ -0,0 +1,9 @@
+data "aws_ssm_parameter" "frontend_alb_sg_id" {
+  name = "/${var.project}/${var.environment}/frontend_alb_sg_id"
+}
+data "aws_ssm_parameter" "public_subnet_ids" {
+  name = "/${var.project}/${var.environment}/public-subnet-ids"
+}
+data "aws_ssm_parameter" "certificate_arn" {
+  name = "/${var.project}/${var.environment}/certificate_arn"
+}
---
+++ 80-frontend-alb/varaibles.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_id" {
+  default = "Z0259442CKV1OWK97ZR9"
+}
+
+variable "domain_name" {
+  default = "vinaydevops.online"
+}
