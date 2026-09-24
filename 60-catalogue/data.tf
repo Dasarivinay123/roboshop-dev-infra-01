7de927c2975e8f3cfcba774e9354558318c6b19f
Vinay Dasari <dasariv211@gmail.com>
2026-09-24 13:06:44 +0530

    terraform

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -7,6 +7,9 @@ data "aws_ssm_parameter" "private_subnet_ids" {
 data "aws_ssm_parameter" "vpc_id" {
    name  = "/${var.project}/${var.environment}/vpc-id"
 }
+data "aws_ssm_parameter" "backend_alb_listener_arn" {
+    name  = "/${var.project}/${var.environment}/backend_alb_listener_arn"
+}
 data "aws_ami" "joindevops" {
   most_recent = true
   owners      = ["973714476881"]
