db2e99bf5c6bc0feed2375d059e7c9ff4027d39f
Vinay Dasari <dasariv211@gmail.com>
2026-09-24 13:47:47 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -181,3 +181,14 @@ resource "aws_lb_listener_rule" "catalogue" {
     }
   }
 }
+resource "terraform_data" "catalogue_delete" {
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
+  depends_on = [aws_autoscaling_policy.catalogue]
+
+  # executes where terraform is running
+  provisioner "local-exec" {
+    command = "aws ec2 terminate-instances --instance-ids ${aws_instance.catalogue.id}"
+  }
+}
