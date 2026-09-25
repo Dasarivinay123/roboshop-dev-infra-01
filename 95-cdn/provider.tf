b3f8d7cfa15c94fb755ed7ccabb058feeb94d080
Vinay Dasari <dasariv211@gmail.com>
2026-09-25 11:48:48 +0530

    terraform

--- 95-cdn/provider.tf
+++ 95-cdn/provider.tf
@@ -10,7 +10,7 @@ terraform {
   # remote state storage location
   backend "s3" {
     bucket       = "remote-state-vinay"
-    key          = "roboshop-components.tfstate"
+    key          = "roboshop-cdn.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true # Enables native S3 state locking (Terraform 1.10+)
